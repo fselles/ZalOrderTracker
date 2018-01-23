1509d8b3628a7302b02a7e2706bcdfd59ec2750d
Frans Selles <frans.selles@gmail.com>
2018-01-23 16:50:55 +0100

    deploy first version

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ before_action :require_signin
 
   def create
     @order = Order.find(params[:order_id])
-    @item = @order.items.create(params[:item].permit(:article_number, :article_description :quantity, :price, :hrpdf_url, :item_comments))
+    @item = @order.items.create(params[:item].permit(:article_number, :article_description, :quantity, :price, :hrpdf_url, :item_comments))
   end
 
 end
